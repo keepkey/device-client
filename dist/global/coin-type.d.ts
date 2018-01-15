51c8af3ceb1c8381f777c0077f082a0ab8206487
Nick Miller <nick@keepkey.com>
2018-01-15 12:15:31 -0700

    erc20: add isToken field to CoinType for client-side filtering

--- dist/global/coin-type.d.ts
+++ dist/global/coin-type.d.ts
@@ -6,6 +6,7 @@ export interface CoinTypeConfiguration {
     name: string;
     currencySymbol: string;
     coinTypeCode: string;
+    isToken: boolean;
     addressFormat: string;
     dust: number | string;
     decimals: number;
