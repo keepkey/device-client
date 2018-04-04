fd95ca5f20efbd4c1e63ff0e08344b13652406f3
Hodler <ken@keepkey.com>
2018-04-04 16:31:18 -0600

    Cleanup: Remove duplicate hashes Cleanup: Move model detection to the device profiles

--- src/features-service.ts
+++ src/features-service.ts
@@ -49,21 +49,6 @@ export class FeaturesService {
     features.coin_metadata = CoinType.getList().map((coin:CoinType) => coin.toFeatureCoinMetadata());
     features.version = `v${features.major_version}.${features.minor_version}.${features.patch_version}`;
 
-    if (features.bootloader_mode) {
-      // Override the version number for older devices that don't have a model number specified
-      switch (features.version) {
-        case 'v1.0.0':
-        case 'v1.0.1':
-        case 'v1.0.2':
-        case 'v1.0.3':
-          features.model = 'K1-14AM';
-          break;
-        case 'v1.0.4':
-          features.model = 'K1-14WL-S';
-          break;
-      }
-    }
-
     if (!features.model) {
       features.model = 'K1-14AM';
     }
