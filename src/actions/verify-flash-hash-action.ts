b5f28685a789b134c7842016190c9748173ed70c
Hodler <ken@keepkey.com>
2018-04-04 16:44:51 -0600

    Add mfg firmware detection Change the validateRWSector function to use sha3 instead of sha256 Rename VerifyFlashHash to VerifyFlashHashAction Add the VerifyFlashHashAction to DeviceClient as verifyDevice()

--- src/actions/verify-flash-hash-action.ts
+++ src/actions/verify-flash-hash-action.ts
@@ -1,5 +1,6 @@
 import {DeviceClient} from "../device-client";
 import {Features} from "../global/features";
+import {sha3_256} from 'js-sha3';
 import * as Bitcore from "bitcore-lib";
 import ByteBuffer = require('bytebuffer');
 import {DeviceMessageHelper} from "../device-message-helper";
@@ -16,7 +17,7 @@ type SectorData = {
 const CHUNK_SIZE = 64;
 const CHALLENGE_SIZE = 32;
 
-export class VerifyFlashHash {
+export class VerifyFlashHashAction {
   private static sectors: Array<SectorData> = [
     {start: 0x08004000, end: 0x08007FFF},
     {start: 0x08008000, end: 0x0800BFFF},
@@ -26,26 +27,21 @@ export class VerifyFlashHash {
     {start: 0x080E0000, end: 0x080FFFFF}
   ];
 
-  public static operation(client: DeviceClient,
-                          challenge?: ByteBuffer): Promise<any> {
+  public static operation(client: DeviceClient): Promise<any> {
+    let challenge = ByteBuffer.wrap(Bitcore.crypto.Random.getRandomBuffer(CHALLENGE_SIZE));
     return client.featuresService.promise
       .then((features: Features) => {
-        if (!challenge) {
-
-          challenge = ByteBuffer.wrap(Bitcore.crypto.Random.getRandomBuffer(CHALLENGE_SIZE));
-        }
-
         let promise = Promise.resolve();
-        VerifyFlashHash.sectors.forEach((sectorData: SectorData) => {
-          let length = VerifyFlashHash.sectorLenth(sectorData);
+        VerifyFlashHashAction.sectors.forEach((sectorData: SectorData) => {
+          let length = VerifyFlashHashAction.sectorLength(sectorData);
           sectorData.buffer = ByteBuffer.wrap(Bitcore.crypto.Random.getRandomBuffer(length));
-          promise = promise.then(() => VerifyFlashHash.writeSector(client, sectorData));
+          promise = promise.then(() => VerifyFlashHashAction.writeSector(client, sectorData));
         });
         return promise;
       })
       .then(() => {
-        VerifyFlashHash.sectors.forEach((sectorData: SectorData) =>
-          VerifyFlashHash.validateRWSector(client, sectorData, challenge));
+        VerifyFlashHashAction.sectors.forEach((sectorData: SectorData) =>
+          VerifyFlashHashAction.validateRWSector(client, sectorData, challenge));
       });
   }
 
@@ -65,22 +61,21 @@ export class VerifyFlashHash {
   }
 
   private static validateRWSector(client: DeviceClient, sector: SectorData, challenge: ByteBuffer) {
-    let dataToHash: ByteBuffer = new ByteBuffer(CHALLENGE_SIZE + VerifyFlashHash.sectorLenth(sector));
+    let dataToHash: ByteBuffer = new ByteBuffer(CHALLENGE_SIZE + VerifyFlashHashAction.sectorLength(sector));
 
     console.assert(challenge.capacity() === CHALLENGE_SIZE, "Challenge buffer size is wrong");
-    console.assert(sector.buffer === VerifyFlashHash.sectorLenth(sector), "sector data buffer size is wrong");
+    console.assert(sector.buffer.capacity() === VerifyFlashHashAction.sectorLength(sector), "sector data buffer size is wrong");
 
     dataToHash
       .append(challenge)
       .append(sector.buffer);
 
-    let expectedResponse = ByteBuffer.wrap(
-      Bitcore.crypto.Hash.sha256(dataToHash.toBuffer())).toHex();
+    let expectedResponse = sha3_256(dataToHash.toArrayBuffer());
 
     let message: FlashHash = DeviceMessageHelper.factory('FlashHash');
     message.setAddress(sector.start);
     message.setChallenge(challenge);
-    message.setLength(VerifyFlashHash.sectorLenth(sector));
+    message.setLength(VerifyFlashHashAction.sectorLength(sector));
 
     return client.writeToDevice(message)
       .then((response: FlashHashResponse) => {
@@ -92,7 +87,7 @@ export class VerifyFlashHash {
       })
   }
 
-  private static sectorLenth(sectorData) {
+  private static sectorLength(sectorData) {
     return sectorData.end - sectorData.start;
   }
 }
