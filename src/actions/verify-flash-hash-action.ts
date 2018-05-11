2fdbd839612c6c60d927520b95bcb1992187709c
Ryan <ryan@keepkey.com>
2018-05-11 14:52:01 -0600

    WIP: figure out why fingerprint doesn't match flash fingerprints

--- src/actions/verify-flash-hash-action.ts
+++ src/actions/verify-flash-hash-action.ts
@@ -14,7 +14,6 @@ export type SectorData = {
   name: string,
   start: number,
   end: number,
-  ro?: boolean,
   buffer?: ByteBuffer,
 }
 
@@ -22,6 +21,7 @@ export enum VerifyFlashSteps {
   starting,
   createRandomData,
   writeRandomData,
+  findB64Asset,
   sectorVerification,
 }
 
@@ -47,14 +47,18 @@ const MAX_BYTES_OF_ENTROPY_AVAILABLE = 65536;
 
 export class VerifyFlashHashAction {
   private static rwSectors: Array<SectorData> = [
-    {name: 'bootstrap', start: 0x08000000 , end: 0x08003FFF, ro: true},
     {name: 'empty-sector-1', start: 0x08004000, end: 0x08007FFF},
     {name: 'empty-sector-2', start: 0x08008000, end: 0x0800BFFF},
     {name: 'data-sector', start: 0x0800C000, end: 0x0800FFFF},
     {name: 'empty-sector-4', start: 0x08010000, end: 0x0801FFFF},
-    {name: 'bootloaders', start: 0x08020000 , end: 0x0805FFFF, ro: true},
     // (0x080C0f32,0x080DFFFF),
     {name: 'reserved-sector', start: 0x080E0000, end: 0x080FFFFF}
+  ];
+
+  private static roSectors: Array<SectorData> = [
+    {name: 'bootstrap', start: 0x08000000, end: 0x08003FFF},
+    {name: 'bootloaders-1', start: 0x08020000, end: 0x0803FFFF},
+    {name: 'bootloaders-2', start: 0x08040000, end: 0x0805FFFF},
   ];
 
 // flash memory layout:
@@ -95,43 +99,44 @@ export class VerifyFlashHashAction {
             sector: _.omit(sectorData, 'buffer'),
           });
 
-          // skip for read-only sectors
-          if (sectorData.ro) { return promise; }
-
           while (remaining > 0) {
             let requestedBytes = Math.min(remaining, MAX_BYTES_OF_ENTROPY_AVAILABLE);
             sectorData.buffer.append(Bitcore.crypto.Random.getRandomBuffer(requestedBytes));
             remaining -= requestedBytes;
           }
-          promise = promise.then(() => VerifyFlashHashAction.writeSector(client, sectorData, protectedStatusCallback));
+          promise = promise.then(() => VerifyFlashHashAction.writeSector(client, sectorData, protectedStatusCallback))
         });
         return promise;
       })
       .then(() => {
-        return Promise.all(VerifyFlashHashAction.rwSectors.map((sectorData: SectorData) => {
-          let result: VerifyFlashResult;
-          return VerifyFlashHashAction.validateRWSector(client, sectorData)
-            .then(() => result = VerifyFlashResult.successful)
-            .catch((msg) => {
-              console.error(msg);
-              result = VerifyFlashResult.failure;
-            })
-            .then(() => {
-              protectedStatusCallback({
-                step: VerifyFlashSteps.sectorVerification,
-                result: result,
-                sector: _.omit(sectorData, 'buffer')
-              });
-            })
-        }));
-      });
+        let promise = Promise.resolve();
+        VerifyFlashHashAction.roSectors.map((sectorData: SectorData) => {
+          promise = promise.then(() => VerifyFlashHashAction.fetchB64Assets(client, sectorData, statusCallback))
+        });
+        return promise;
+      })
+      // .then(() => {
+      //   return Promise.all(VerifyFlashHashAction.rwSectors.map((sectorData: SectorData) => {
+      //     let result: VerifyFlashResult;
+      //     return VerifyFlashHashAction.validateRWSector(client, sectorData)
+      //       .then(() => result = VerifyFlashResult.successful)
+      //       .catch((msg) => {
+      //         console.error(msg);
+      //         result = VerifyFlashResult.failure;
+      //       })
+      //       .then(() => {
+      //         protectedStatusCallback({
+      //           step: VerifyFlashSteps.sectorVerification,
+      //           result: result,
+      //           sector: _.omit(sectorData, 'buffer')
+      //         });
+      //       });
+      //   }));
+      // });
   }
 
   private static writeSector(client: DeviceClient, sectorData: SectorData, statusCallback: StatusCallbackFunction): Promise<any> {
     let promise = Promise.resolve();
-
-    // skip for read-only sectors
-    if (sectorData.ro) { return promise; }
 
     for (let pos = sectorData.start; pos <= sectorData.end; pos += MAX_CHUNK_SIZE) {
       let message: FlashWrite = DeviceMessageHelper.factory('FlashWrite');
@@ -156,8 +161,7 @@ export class VerifyFlashHashAction {
               size: chunkSize,
             });
 
-          })
-          .then(() => VerifyFlashHashAction.fetchB64Assets(client, sectorData, statusCallback));
+          });
       });
     }
     return promise;
@@ -174,8 +178,41 @@ export class VerifyFlashHashAction {
     return client.writeToDevice(message)
       .then((response: FlashHashResponse) => {
         let fingerprint = response.data.toHex();
+        console.log('FINGERPRINT FROM DEVICE', fingerprint);
+        let promise = Promise.resolve();
 
-      })
+        promise = promise.then(() => {
+          statusCallback({
+            step: VerifyFlashSteps.findB64Asset,
+            result: VerifyFlashResult.inProgress,
+            sector: _.omit(sector, 'buffer')
+          });
+        });
+
+        // promise = promise.then(() => VerifyFlashHashAction.findB64asset(sector, flashBinaries, fingerprint));
+        return promise;
+      });
+  }
+
+  private static findB64asset(sector: SectorData, obj: object, fingerprint: string): Promise<void> {
+    let promise = Promise.resolve()
+
+    for (var item in obj) {
+      if (obj.hasOwnProperty(item)){
+        var expected = obj[item];
+        if (expected === fingerprint) {
+            console.log('FOUND FINGERPRINT!', fingerprint)
+           // sector.base64asset = expected['b64_asset'];
+           break;
+        }
+        if (typeof obj !== 'object') { continue; }
+        console.log('fingerprint device', fingerprint);
+        console.log('fingerprint database', expected);
+        promise = promise.then(() => VerifyFlashHashAction.findB64asset(expected, obj, fingerprint));
+      }
+      break;
+    }
+    return promise;
   }
 
   private static validateRWSector(client: DeviceClient, sector: SectorData): Promise<void> {
