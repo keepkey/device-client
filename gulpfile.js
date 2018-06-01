29d682eea2c1ab981eecd438b52dc6546a741eb1
Hodler <ken@keepkey.com>
2018-06-01 14:54:43 -0600

    Add the device sector map and the device-sector-provider that does some checking before using it Move checked in file from the dist directory.

--- gulpfile.js
+++ gulpfile.js
@@ -44,7 +44,8 @@ const paths = {
   typescriptSources: [
     'src/**/*.ts', '!src/**/*.spec.ts'
   ],
-  versionedFiles: ['package.json', 'README.md']
+  versionedFiles: ['package.json', 'README.md'],
+  deviceValidationData: 'device-profiles/device-validation-data/**/*',
 };
 
 gulp.task('buildDeviceProfiles', function gatherConfigs() {
@@ -218,12 +219,42 @@ function copyDtsToDist() {
     .pipe(gulp.dest(paths.distDirectory));
 }
 
-gulp.task('pre-tsc', gulp.series(
+function copyValidationDataToDist() {
+  return gulp.src(paths.deviceValidationData)
+    .pipe(gulp.dest(paths.distDirectory));
+}
+
+gulp.task('validateDeviceSectorMap', () => {
+  let DeviceSectorProvider = require('./dist/global/device-sector-provider');
+  return gulp.src(paths.messagesJs)
+    .pipe(through.obj((file, enc, callback) => {
+      let error;
+      try {
+        DeviceSectorProvider.DeviceSectorProvider.getInstance();
+      } catch (e) {
+        error = e;
+        this.emit('error', new PluginError(PLUGIN_NAME, e));
+      } finally {
+        callback(error, file);
+      }
+    }));
+});
+
+function tsc() {
+  return gulp.src(paths.typescriptSources)
+    .pipe(ts(require('./tsconfig.json').compilerOptions)).js
+    .pipe(gulp.dest(paths.distDirectory));
+}
+
+gulp.task('build', gulp.series(
+  copyValidationDataToDist,
   'buildDeviceProfiles',
   'buildBootloaderProfiles',
   'extractMetadataFromFirmware',
   'messages.d.ts',
-  copyDtsToDist
+  copyDtsToDist,
+  tsc,
+  'validateDeviceSectorMap'
 ));
 
 module.exports = gulp;
